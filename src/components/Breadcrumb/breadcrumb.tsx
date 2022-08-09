85ece9fecb74dcd329e12309796c0a369f6cf968
Kenechukwu <kenechukwu.ajufo@gmail.com>
2022-08-09 16:40:59 +0100

    Check href to link component in breadcrumb

--- src/components/Breadcrumb/breadcrumb.tsx
+++ src/components/Breadcrumb/breadcrumb.tsx
@@ -1,9 +1,10 @@
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Link } from 'react-router-dom';
 
 function BreadCrumb() {
   return (
     <Breadcrumb>
-      <Breadcrumb.Item href="/users">User List</Breadcrumb.Item>
+      <Breadcrumb.Item><Link to="/users">User List</Link></Breadcrumb.Item>
       <Breadcrumb.Item active>User Details</Breadcrumb.Item>
     </Breadcrumb>
   );
